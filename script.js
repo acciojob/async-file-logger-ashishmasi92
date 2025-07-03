e2ab29ccf6701068089a92814cc95ed688000300
Ashish <ashishmasih92g@gmail.com>
2025-07-03 13:37:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,5 +7,22 @@ const fileNames = [
   "file5.txt",
 ];
 
+async function hello(){
+
+	try {
+		for(let i=0;i<=fileNames.length-1;i++){
+			let x = await fetch(fileNames[i])
+			if(!x.ok){
+				 throw new Error("Failed to fetch " + fileNames[i]);
+			}
+
+			let content = await x.text()
+			console.log(content)
+		}
+	} catch (error) {
+		console.log(error.message)
+	}
+}
+hello()
 // do not change the code above
 // add your code here
